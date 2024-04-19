9abdc26dbe4b6b0fd5440146c91b8e6aec2ed00e
Edward Thang <edward.thang@admenergy.com>
2024-04-19 12:52:54 -0700

    jest

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,5 +1,10 @@
 const esModules = ["jsondiffpatch"];
 const customConfig = {
+  preset: "ts-jest",
+  moduleDirectories: ["node_modules", "src"], // Add src to module directories
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1", // Map TypeScript paths to Jest paths
+  },
   testEnvironment: "jest-environment-jsdom",
   testMatch: ["<rootDir>/__tests__/**/*.test.(js|jsx)"],
   transform: {
