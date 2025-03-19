a41203996baf5004a70f5a5de3e48f63a6cfb56f
Edward Thang <project@nyaarium.com>
2025-03-19 11:05:21 -0700

    Split CJS & ESM in Webpack

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,43 +1,7 @@
-import { CacheProvider } from "@emotion/react";
-import CssBaseline from "@mui/material/CssBaseline";
-import { ThemeProvider } from "@mui/material/styles";
 import { RemixBrowser } from "@remix-run/react";
-import { ReactNode, startTransition, useMemo, useState } from "react";
+import { startTransition } from "react";
 import { hydrateRoot } from "react-dom/client";
-import ClientStyleContext from "~/styles/ClientStyleContext";
-import createEmotionCache from "~/styles/createEmotionCache";
-import theme from "~/styles/theme";
-
-interface ClientCacheProviderProps {
-  children: ReactNode;
-}
-function ClientCacheProvider({ children }: ClientCacheProviderProps) {
-  const [cache, setCache] = useState(createEmotionCache());
-
-  const clientStyleContextValue = useMemo(
-    () => ({
-      reset() {
-        setCache(createEmotionCache());
-      },
-    }),
-    [],
-  );
-
-  return (
-    <ClientStyleContext.Provider value={clientStyleContextValue}>
-      <CacheProvider value={cache}>{children}</CacheProvider>
-    </ClientStyleContext.Provider>
-  );
-}
 
 startTransition(() => {
-  hydrateRoot(
-    document,
-    <ClientCacheProvider>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <RemixBrowser />
-      </ThemeProvider>
-    </ClientCacheProvider>,
-  );
+  hydrateRoot(document, <RemixBrowser />);
 });
