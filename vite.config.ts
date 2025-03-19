ffe357980aa77899c88ad4bfec6a914bdbbfe9a1
Edward Thang <project@nyaarium.com>
2025-03-19 08:59:37 -0700

    Fixing MUI

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,50 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import type { ConfigEnv, UserConfig } from "vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { configDefaults } from "vitest/config";
 
-export default ({ command }: any) => {
+const TEST = process.env.NODE_ENV === "test";
+
+export default async ({ command }: ConfigEnv): Promise<UserConfig> => {
+  const tsconfigPaths = (await import("vite-tsconfig-paths")).default;
+
   const ssr = {
     external: ["argon2"],
     noExternal: [],
   };
 
-  return defineConfig({
+  return {
     server: {
       host: true,
-      port: 3000, // Development port must be set here
+      port: 3000,
     },
-    plugins: [
-      remix({
-        future: {
-          v3_fetcherPersist: true,
-          v3_lazyRouteDiscovery: true,
-          v3_relativeSplatPath: true,
-          v3_singleFetch: true,
-          v3_throwAbortReason: true,
-        },
-      }),
-      tsconfigPaths(),
-    ],
+    plugins: TEST
+      ? [tsconfigPaths()]
+      : [
+          remix({
+            future: {
+              v3_fetcherPersist: true,
+              v3_lazyRouteDiscovery: true,
+              v3_relativeSplatPath: true,
+              v3_singleFetch: true,
+              v3_throwAbortReason: true,
+            },
+          }),
+          tsconfigPaths(),
+        ],
     test: {
       globals: true,
-      environment: "node",
-      include: ["app/**/*.test.ts"],
+      environment: "jsdom",
+      include: ["app/**/*.test.ts", "app/**/*.test.tsx"],
       exclude: configDefaults.exclude,
       coverage: {
         provider: "v8",
         reporter: ["text", "json", "html"],
-        include: ["app/**/*.ts"],
-        exclude: ["app/**/*.test.ts"],
+        include: ["app/**/*.ts", "app/**/*.tsx"],
+        exclude: ["app/**/*.test.ts", "app/**/*.test.tsx"],
       },
       pool: "forks",
     },
     ssr,
-  });
+  };
 };
